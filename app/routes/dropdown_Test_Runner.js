58a70e7100f514ab766d1fb52a66b4da0965f5d1
James <jamessandoval>
2018-04-16 08:28:42 -0700

    merge latest updates.

--- app/routes/dropdown_Test_Runner.js
+++ app/routes/dropdown_Test_Runner.js
@@ -15,17 +15,233 @@ const path = require('path');
 let rootPath = path.normalize(__dirname + '../../../');
 rootPath = rootPath + 'temp_directory';
 
+/*######################################
+##
+## Check to see if a process is running
+## Uses promises and a call to system 
+## PS grep "keyword to check if running"
+## Parameters 
+##
+## 1. ProcessName ie. java
+## 2. nameToMatch ie. selenium
+##
+####################################### */
+
+function checkProcessByName(processName, nameToMatch) {
+  return new Promise(function(resolve, reject) {
+    let processList = new Array();
+
+    const { spawn } = require('child_process');
+    const ps = spawn('ps', ['ax']);
+    const grep = spawn('grep', [processName]);
+
+    ps.stdout.on('data', (data) => {
+      grep.stdin.write(data);
+    });
+
+    ps.stderr.on('data', (data) => {
+      console.log(`ps stderr: ${data}`);
+    });
+
+    ps.on('close', (code) => {
+      if (code !== 0) {
+        console.log(`ps process exited with code ${code}`);
+      }
+      grep.stdin.end();
+    });
+
+    // get output 
+    grep.stdout.on('data', (data) => {
+      let output = data.toString();
+      processList.push(output);
+
+    });
+
+    grep.stderr.on('data', (data) => {
+      console.log(`grep stderr: ${data}`);
+    });
+
+    grep.on('close', (code) => {
+
+      let matchFlag = 0;
+
+      for (var i = 0; i < processList.length; i++) {
+
+        if (processList[i].includes(nameToMatch)) {
+          matchFlag += 1;
+        }
+      }
+      if (matchFlag) {
+        //console.log("We really have a match");	
+        resolve("success");
+      } else {
+        reject("fail");
+      }
+
+    });
+  })
+}
+
+function startProcess(processToStart) {
+
+  //
+  // Startup process to start if not running
+  //
+
+  if (processToStart === "selenium") {
+    let spawn = require('child_process').spawn,
+      script = spawn('selenium-standalone', ['start'], { detached: true });
+
+    script.stderr.on('data', (data) => {
+      console.log(`ps stderr: ${data}`);
+    });
+
+    script.on('close', (code) => {
+      if (code !== 0) {
+        console.log(`start Script process exited with code ${code}`);
+      }
+      script.stdin.end();
+    });
+
+    //
+    // Startup phantom js if not running
+    //
+  } else if (processToStart === "phantomjs") {
+
+    let spawn = require('child_process').spawn,
+      script = spawn('phantomjs', ['--webdriver=8643'], { detached: true });
+
+    script.stderr.on('data', (data) => {
+      console.log(`ps stderr: ${data}`);
+    });
+
+    script.on('close', (code) => {
+      if (code !== 0) {
+        console.log(`start Script process exited with code ${code}`);
+      }
+      script.stdin.end();
+    });
+  }
+}
+
+function checkEnvironmentSettings() {
+
+  return new Promise(function(resolve, reject) {
+
+    let process1 = "selenium";
+    let keyword1 = "java";
+    let process2 = "phantomjs";
+    let keyword2 = "phantom";
+
+    let statusObject = {
+      phantom: "on",
+      selenium: "on"
+    };
+
+    // Check to see if Selenium is Running
+    // Start Selenium if not running 	
+
+    checkProcessByName(keyword1, process1).then(response => {
+
+      console.log("process: " + process1 + " is running");
+
+    }).catch(function(response) {
+
+      console.log("selenium is not running...starting");
+
+      startProcess(process1);
+
+    });
+
+    // Check to see if Phantom js running 
+
+    checkProcessByName(keyword2, process2).then(response => {
+
+      console.log("process: " + process2 + " is running");
+
+    }).catch(function(response) {
+
+      console.log("phantomjs is not running...starting");
+
+      startProcess(process2);
+
+    });
+
+    if (statusObject.phantom == "off" && statusObject.selenium == "off") {
+
+      reject(statusObject);
+    } else {
+
+      resolve(statusObject);
+    }
+  })
+
+};
 
 // OLD Export tool
 exports.getOverview = function(req, res) {
 
+  checkEnvironmentSettings().then(statusObject => {
+
     res.render('dropdownTestRunner', {
       title: 'Run Tests',
-      // features: features,
-      // languages: languages,
+      driverStatus: statusObject,
       user: req.user.firstname
 
     });
 
+  })
+
 };
 
+
+
+// Promise example ::
+
+
+/*
+
+function checkProcessByName(processName, nameToMatch) {
+  return new Promise(function(resolve, reject) {
+
+
+  	getData(someValue, function(error, result){
+
+
+
+
+
+  		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if(error){
+                reject(error);
+            }
+            else{
+                resolve(result);
+            }
+        })
+
+
+
+
+
+
+  })
+
+
+
+*/
--- public/js/runTest.js
+++ public/js/runTest.js
@@ -21,6 +21,8 @@ function runTest() {
   */
 
   var obj = { "name": "F2", "locale": "en-us", "numberOfUrls": 3, "testCases": [0] };
+
+  
   arrayOfObjects.push(obj);
 
   /*
@@ -70,7 +72,6 @@ function runTest() {
       console.log("I am great success.");
     }
   })
-
 };
 
 function exportLanguageSet() {
@@ -146,11 +147,10 @@ function exportAll() {
 function exportSelections() {
 
   let template = '';
-  let language = ''; 
-  let testresult=""; 
-  let query = ""; 
-  let thehref="";
-  let testdate="";
+  let language = '';
+  let testresult = "";
+  let query = "";
+  let thehref = "";
 
   let TchildCount = document.getElementById("pageChildren").children.length;
   let LchildCount = document.getElementById("langChildren").children.length;
@@ -166,24 +166,18 @@ function exportSelections() {
 
 
   language = document.getElementById("langChildren").children[0].id; // this takes the first language child and puts it in 'language'
-  language = language.slice(0,-1);
-  if (language == "LAll"){
-    language = "All"
-  }
+  language = language.slice(0, -1);
 
   for (var y = 1; y < LchildCount; y++) { // if additional languages were chosen, we add a comma and the language for each one selected
     let l = document.getElementById("langChildren").children[y].id;
     l = l.slice(0, -1);
     language = language + "," + l;
   }
-  
-  testdate = document.getElementById("dateChild").children[0].id;
-  testdate = testdate.slice(0, -1);
-  console.log(testdate +"---------------------------------------------------");
+
 
   //the href will contain a list of each languages as 'en-us,de-de' and features will be 'f1,f3,f5' 
   // in the getExportFromResults() function on 'api_export.js' these commas are watched for, so that the string can be split to an array and a query created for all the selections
 
-  thehref="/export?feature="+ template + "&language=" + language + "&testresult=" + testresult + "&query=" + query + "&testpassid=" + testdate;
-  document.getElementById("myhref").href=thehref;
+  thehref = "/export?feature=" + template + "&language=" + language + "&testresult=" + testresult + "&query=" + query;
+  document.getElementById("myhref").href = thehref;
 }
